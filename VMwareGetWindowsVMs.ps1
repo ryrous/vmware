22144c662c3270f8d9911a0d298b08745b397b6f
Ry Ro <ry@ryroberts.com>
2019-09-27 12:13:22 -1000

    powercli

--- VMwareGetWindowsVMs.ps1
+++ VMwareGetWindowsVMs.ps1
@@ -1,3 +1,3 @@
 Get-VM | Where-Object {$_.Guest -like "*Windows*"} `
        | Select-Object Name, @{N="Configured OS";E={$_.ExtensionData.Config.GuestFullname}} `
-       | Export-Csv AWSWindowsVMs.csv -NoTypeInformation -UseCulture
+       | Export-Csv WindowsVMs.csv -NoTypeInformation -UseCulture
