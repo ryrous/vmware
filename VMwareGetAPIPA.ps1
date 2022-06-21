98c05497b5f5f4b0bd4913ac72a09c51438905d8
Ry <ryroberts1@outlook.com>
2022-06-21 06:19:42 -1000

    vmware

--- VMwareGetAPIPA.ps1
+++ VMwareGetAPIPA.ps1
@@ -2,4 +2,4 @@
 Get-View -ViewType VirtualMachine `
          -Property Name, Guest. IPAddress `
          -Filter @{"Guest.IpAddress"="169.254.*"} | Select-Object Name, @{N="IP";E={@($_.Guest.IPAddress)}} `
-                                                  | Export-Csv .\APIPAIPAddresses.csv -NoTypeInformation
+| Export-Csv .\APIPAIPAddresses.csv -NoTypeInformation
