6bd9c9431682ee400e5d736d22a0b08f79c768fc
Ry Ro <ryroberts1@outlook.com>
2022-08-01 16:12:22 -1000

    vmware

--- GetDatastoreUsage.ps1
+++ GetDatastoreUsage.ps1
@@ -1,1 +1,3 @@
-Get-Datastore | Select-Object @{N="DataStoreName";E={$_.Name}},@{N="Percentage Free Space(%)";E={[math]::Round(($_.FreeSpaceGB)/($_.CapacityGB)*100,2)}} | Where-Object {$_."Percentage(<20%)" -le 20} | Export-Csv .\DatastoreUsage.csv -NoTypeInformation
+Get-Datastore | Select-Object @{N="DataStoreName";E={$_.Name}},
+    @{N="Percentage Free Space(%)";E={[math]::Round(($_.FreeSpaceGB)/($_.CapacityGB)*100,2)}} `
+    | Where-Object {$_."Percentage(<20%)" -le 20} | Export-Csv .\DatastoreUsage.csv -NoTypeInformation
