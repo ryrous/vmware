01c5deb552e07075824b04d2d831bf934f4075f0
ryrous <ryroberts1@outlook.com>
2023-06-02 09:05:19 -1000

    vmware

--- GetSQLVersion.ps1
+++ GetSQLVersion.ps1
@@ -2,11 +2,14 @@ $VM = Import-Csv -Path .\VMs.csv
 $User = "Administrator"
 $Pass = "Password"
 
-Invoke-VMScript -ScriptText {
-    $inst = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances
+$Script = '
+    $inst = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server").InstalledInstances
     ForEach ($i in $inst) {
-        $p = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL').$i
+        $p = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL").$i
         (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$p\Setup").Edition
         (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$p\Setup").PatchLevel
-    }
-} -VM $VM -GuestUser $User -GuestPassword $Pass -ScriptType Powershell
+    }'
+ 
+foreach ($VM in $VMs) {
+    Invoke-VMScript -ScriptText $Script -VM $VM.Name -GuestUser $User -GuestPassword $Pass -ScriptType Powershell | Export-Csv -Path .\SQLVersions.csv -Append -NoTypeInformation
+}
