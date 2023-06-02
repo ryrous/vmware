7377a41c4210c8587d1028016f8a55db16cc230a
ryrous <ryroberts1@outlook.com>
2023-06-02 11:15:24 -1000

    vmware

--- GetFQDN.ps1
+++ GetFQDN.ps1
@@ -1,1 +1,4 @@
-Get-VM | Select-Object Name, @{N='FQDN';E={$_.ExtensionData.Guest.IPStack[0].DnsConfig.HostName,$_.ExtensionData.Guest.IPStack[0].DnsConfig.DomainName -join '.'}}
+$VMs = Import-Csv -Path .\VMs.csv
+foreach ($VM in $VMs) {
+    Get-VM -Name $VM.Name | Select-Object Name, @{N='FQDN';E={$_.ExtensionData.Guest.IPStack[0].DnsConfig.HostName,$_.ExtensionData.Guest.IPStack[0].DnsConfig.DomainName -join '.'}} | Export-Csv -Path .\FQDN.csv -Append -NoTypeInformation
+}
