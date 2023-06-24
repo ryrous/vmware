59a5a3f5ef77d76bd190a41b72da29231ffa21f5
ryrous <ryrous@outlook.com>
2023-06-23 16:56:32 -1000

    powershell

--- GetNetInfo.ps1
+++ GetNetInfo.ps1
@@ -1,5 +1,4 @@
-Get-VM -Name * | Where-Object {$_.PowerState -eq "PoweredOn"} | Select-Object Name | Export-Csv -Path .\VMlist.csv -NoTypeInformation
-$VMs = Import-Csv -Path .\VMlist.csv
+$VMs = Get-VM -Name * | Where-Object {$_.PowerState -eq "PoweredOn"} | Select-Object Name 
 foreach ($VM in $VMs){
-    Get-VMHostNetworkAdapter -VMKernel | Select-Object VMhost, vmName, Name, Domain, IP, SubnetMask, Gateway, DNS1, DNS2,  Mac, PortGroupName, vMotionEnabled, mtu, FullDuplex, BitRatePerSec | Export-Csv .\NetworkInfo.csv -Append -NoTypeInformation
+    Get-VMHostNetworkAdapter -VMKernel | Select-Object VMhost, vmName, Name, Domain, IP, SubnetMask, Gateway, DNS1, DNS2, Mac, PortGroupName, vMotionEnabled, mtu, FullDuplex, BitRatePerSec | Export-Csv .\NetworkInfo.csv -Append -NoTypeInformation
 }
