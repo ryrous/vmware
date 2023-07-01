2197aad1afc3eb12401e025eaa05568db77d1b1e
ryrous <ryrous@outlook.com>
2023-06-30 16:50:44 -1000

    vmware

--- GetE1000Adapters.ps1
+++ GetE1000Adapters.ps1
@@ -2,7 +2,8 @@ $report = @()
 foreach($cluster in Get-Cluster){
     foreach($rp in Get-ResourcePool -Location $cluster){
         foreach($vm in (Get-VM -Location $rp | Where-Object{Get-NetworkAdapter -VM $_ | Where-Object{$_.Type -eq 'e1000'}})){
-            $report += $vm | Select-Object @{N='VM';E={$_.Name}},
+            $report += $vm | Select-Object `
+                @{N='VM';E={$_.Name}},
                 @{N='vCenter';E={$_.Uid.Split('@')[1].Split(':')[0]}},
                 @{N='Cluster';E={$cluster.Name}},
                 @{N='ResourcePool';E={$rp.Name}}
