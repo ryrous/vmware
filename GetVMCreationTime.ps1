de0a7d725c06e426a1040e0198fd06f8759dbcbd
ryrous <ryrous@outlook.com>
2023-06-30 17:05:28 -1000

    vmware

--- GetVMCreationTime.ps1
+++ GetVMCreationTime.ps1
@@ -1,10 +1,16 @@
 $vmName = '*'
 $eventTYpes = 'VmCreatedEvent', 'VmClonedEvent', 'VmDeployedEvent', 'VmRegisteredEvent'
-
 Get-VM -Name $vmName | ForEach-Object -Process {
    Get-VIEvent -Entity $_ -MaxSamples ([int]::MaxValue) | `
    Where-Object {$eventTYpes -contains $_.GetType().Name} | `
    Sort-Object -Property CreatedTime -Descending | `
    Select-Object -First 1 | `
-   ForEach-Object -Process {New-Object PSObject -Property ([ordered]@{VM = $_.VM.Name; CreatedTime = $_.CreatedTime; User = $_.UserName; EventType = $_.GetType().Name})}
+   ForEach-Object -Process {
+      New-Object PSObject -Property ([ordered]@{
+         VM = $_.VM.Name; `
+         CreatedTime = $_.CreatedTime; `
+         User = $_.UserName; `
+         EventType = $_.GetType().Name `
+      })
+   } | Format-Table -AutoSize
 }
