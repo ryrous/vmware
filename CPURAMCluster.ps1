0b84fbbb6f11201136687760d30ea77e7b1d9204
ryrous <ryrous@outlook.com>
2023-07-17 07:15:58 -1000

    vmware

--- CPURAMCluster.ps1
+++ CPURAMCluster.ps1
@@ -2,16 +2,16 @@ $AllClusters = @()
 Get-Cluster * | ForEach-Object {
     $clusstat = "" | Select-Object ClusterName, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin
     $clusstat.ClusterName = $_.Name
-    $statcpu = Get-Stat -Entity $_ -Start (get-date).AddDays(-7) -Finish (Get-Date)-MaxSamples 100 -Stat cpu.usage.average
-    $statmem = Get-Stat -Entity $_ -Start (get-date).AddDays(-7) -Finish (Get-Date)-MaxSamples 100 -Stat mem.usage.average
+    $statcpu = Get-Stat -Entity $_ -Start (get-date).AddDays(-30) -Finish (Get-Date)-MaxSamples 1000 -Stat cpu.usage.average
+    $statmem = Get-Stat -Entity $_ -Start (get-date).AddDays(-30) -Finish (Get-Date)-MaxSamples 1000 -Stat mem.usage.average
     $cpu = $statcpu | Measure-Object -Property Value -Average -Maximum -Minimum
     $mem = $statmem | Measure-Object -Property Value -Average -Maximum -Minimum
-    $clusstat.CPUMax = $cpu.Maximum
-    $clusstat.CPUAvg = $cpu.Average
-    $clusstat.CPUMin = $cpu.Minimum
-    $clusstat.MemMax = $mem.Maximum
-    $clusstat.MemAvg = $mem.Average
-    $clusstat.MemMin = $mem.Minimum
+    $clusstat.CPUMax = [math]::Round($cpu.Maximum)
+    $clusstat.CPUAvg = [math]::Round($cpu.Average)
+    $clusstat.CPUMin = [math]::Round($cpu.Minimum)
+    $clusstat.MemMax = [math]::Round($mem.Maximum)
+    $clusstat.MemAvg = [math]::Round($mem.Average)
+    $clusstat.MemMin = [math]::Round($mem.Minimum)
     $AllClusters += $clusstat
 }
-$AllClusters | Select-Object ClusterName, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin | Export-Csv .\CPURAMCluster.csv -NoTypeInformation
+$AllClusters | Select-Object ClusterName, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin | Export-Csv .\CPURAMPerfClusters.csv -NoTypeInformation
